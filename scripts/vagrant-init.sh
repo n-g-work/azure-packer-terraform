7f59b5b7e2895ab650b75adfa5248c750832be14
ng <ng-work@outlook.com>
2022-04-09 14:41:40 +0300

    Rename vagrant script

--- scripts/vagrant-init.sh
+++ scripts/vagrant-init.sh
@@ -6,9 +6,19 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 
 ansible-galaxy install -r "${SCRIPTPATH}/../ansible/requirements.yml" --roles-path "${SCRIPTPATH}/../ansible/roles/"
 
-vars_path="${SCRIPTPATH}/../variables.json"
+pathadd() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then PATH="${PATH:+"$PATH:"}$1"; fi
+}
 
-packer validate -var-file "${vars_path}" "${SCRIPTPATH}/../packer/vagrant-ubuntu.json"
+# for wsl
+if grep -q Microsoft /proc/version; then
+  echo "Windows Subsystem for Linux detected"
+  echo "Please specify VirtualBox install directory on Windows (e.g.: /mnt/c/Program Files/Oracle/VirtualBox)"
+  read -p "[/mnt/c/apps/VirtualBox]: " -r virtualbox_installation_path
+  if [ -z "$virtualbox_installation_path" ]; then virtualbox_installation_path='/mnt/c/apps/VirtualBox'; fi
+  pathadd "${virtualbox_installation_path}"
+  export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
+fi
 
 echo "building packer image..."
 packer build -force -var-file "${vars_path}" "${SCRIPTPATH}/../packer/vagrant-ubuntu.json"
